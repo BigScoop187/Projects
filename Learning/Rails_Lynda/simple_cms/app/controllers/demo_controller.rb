3e332890459fc4e90fb59dbf7b2003ee87d080cd
Burt Haworth <burthaworth@gmail.com>
2014-06-26 19:47:24 -0400

    Lynda Rails 5_3
    Learning about redirects

--- Learning/Rails_Lynda/simple_cms/app/controllers/demo_controller.rb
+++ Learning/Rails_Lynda/simple_cms/app/controllers/demo_controller.rb
@@ -3,5 +3,21 @@ class DemoController < ApplicationController
   layout false
   
   def index
+    render('hello')
   end
+  
+  def hello
+    render('index')
+  end
+  
+  # Redirecting using a controller and action
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+  end
+  
+  # Can redirect with a string url
+  def derp
+    redirect_to("http://www.espn.com")
+  end
+  
 end
